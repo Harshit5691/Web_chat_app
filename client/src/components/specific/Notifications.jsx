58d594e67b19b7391879dbf41417249864187960
Harshit5691 <singhalharshit5691@gmail.com>
2024-10-07 23:17:23 +0530

    Refactor UserItem component and add missing avatar and name props

--- client/src/components/specific/Notifications.jsx
+++ client/src/components/specific/Notifications.jsx
@@ -1,9 +1,19 @@
+import { Dialog, DialogTitle, InputAdornment, List, Stack, TextField, Typography } from '@mui/material'
 import React from 'react'
+import { sampleNotifications } from '../../constants/sampleData'
 
 const Notifications = () => {
-  return (
-    <div>Notifications</div>
-  )
+  return <Dialog open>
+      <Stack p={{ xs:"1rem", sm:"2rem"}} maxWidth={"25rem"}>
+        <DialogTitle>Notifications</DialogTitle>
+        {
+          sampleNotifications.length > 0 ? (
+          <></>
+        ) : (
+        <Typography textAllign={"center"}>No Notification</Typography> 
+      )}
+      </Stack>
+  </Dialog>
 }
 
 export default Notifications
--- client/src/constants/sampleData.js
+++ client/src/constants/sampleData.js
@@ -14,3 +14,31 @@ export const sampleChats = [
         members: ["1", "2"],
     },
 ];
+export const sampleUsers = [
+    {
+        avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+        name: "John Doe",
+        _id: "1",
+    },
+    {
+        avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+        name: "Tanmay Saxena",
+        _id: "2",
+    },
+];
+export const sampleNotifications = [
+    {
+        sender:{
+            avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+            name: "John Doe",
+        },
+        _id: "1",
+    },
+    {
+        sender:{
+            avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+            name: "Tanmay Saxena",
+        },
+        _id: "2",
+    },
+];
